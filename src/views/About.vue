22c3ac3dfeab747ed84faaf53c835c3f629ca5f1
bob <ali.abdi.1991@gmail.com>
2021-06-21 07:23:05 +0000

    create views home, aboutand install vue-router 4, add router index

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-center text-gray-600 bg-gray-300 mt-24">
+        <h1>Aboute page</h1>
+    </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-center text-gray-600 bg-gray-300 mt-24">
+        <h1>Home page</h1>
+    </div>
+</template>
