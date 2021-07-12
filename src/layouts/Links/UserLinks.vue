de3355628bddc2281f89b4020b37483c6c1b343f
bob <ali.abdi.1991@gmail.com>
2021-07-12 18:02:02 +0000

    install v-click-away and review layout link

--- src/layouts/Links/UserLinks.vue
+++ src/layouts/Links/UserLinks.vue
@@ -4,7 +4,7 @@
             <div class="relative flex items-center justify-between h-14">
                 <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                     <!-- Mobile menu button-->
-                    <button @click="openMainMenu = !openMainMenu" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-800 hover:text-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                    <button @click="mainMenu = !mainMenu" type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-800 hover:text-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                         <span class="sr-only">Open main menu</span>
                         <!--
                             Icon when menu is closed.
@@ -53,8 +53,8 @@
                     </button>
                     <!-- Profile dropdown -->
                     <div class="ml-3 relative">
-                        <div>
-                            <button @click="openUserMenu = !openUserMenu" type="button" class="bg-gray-50 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-300 focus:ring-gray-200" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                        <div v-click-away="closeUserMenu">
+                            <button @click="userMenu = !userMenu" type="button" class="bg-gray-50 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-300 focus:ring-gray-200" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="sr-only">Open user menu</span>
                                 <img class="h-8 w-8 rounded-full" src="../../assets/ali.jpg"/>
                             </button>
@@ -69,7 +69,7 @@
                             From: "transform opacity-100 scale-100"
                             To: "transform opacity-0 scale-95"
                         -->
-                        <div v-show="openUserMenu" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-gray-50 ring-1 ring-gray-300 ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                        <div v-show="userMenu" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-gray-50 ring-1 ring-gray-300 ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <!-- Active: "bg-gray-100", Not Active: "" -->
                             <a href="#" class="block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
                             <a href="#" class="block px-4 py-2 text-sm text-gray-600" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
@@ -82,7 +82,7 @@
             </div>
         </div>
         <!-- Mobile menu, show/hide based on menu state. -->
-        <div v-show="openMainMenu"  class="sm:hidden" id="mobile-menu">
+        <div v-show="mainMenu"  class="sm:hidden" id="mobile-menu">
             <div class="px-2 pt-2 pb-3 space-y-1">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                 <a href="#" class="bg-gray-50 text-gray-900 block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Dashboard</a>
@@ -94,29 +94,28 @@
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
     name: "AppLayoutLinks",
 
-    data() {
-        return {
-            openMainMenu: false,
-            openUserMenu: false,
-        }
-    },
-
     setup () {
         const store = useStore()
+        const userMenu = ref(false)
+        const mainMenu = ref(false)
         
         const userSignOut = () => store.dispatch('userSignOut')
+        const closeUserMenu = () => userMenu.value = false
 
         store.dispatch('getCart')
 
         return {
             cartCount: computed(() => store.getters.cartCount),
             userSignOut,
+            closeUserMenu,
+            userMenu,
+            mainMenu,
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import router from'./router'
 import store from'./store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueClickAway from "vue3-click-away";
 
 createApp(App)
 .use(router)
 .use(store)
 .use(VueAxios, axios)
+.use(VueClickAway)
 .mount('#app')
